9e1f4973e105e0060d8144a71988ad6f71805de4
jyothishridhar <jyothishridhar0625@gmail.com>
2024-07-09 21:51:42 +0530

    Update requirements.txt

--- SEM_automation.py
+++ SEM_automation.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import os
-import base64
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
@@ -19,46 +18,46 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from openpyxl import Workbook
 from openpyxl.styles import Font, PatternFill
-from io import StringIO
+
 import sqlite3
 import io
 
-# # Database setup
-# conn = sqlite3.connect('users.db')
-# c = conn.cursor()
+# Database setup
+conn = sqlite3.connect('users.db')
+c = conn.cursor()
 
-# # Create table
-# c.execute('''
-#           CREATE TABLE IF NOT EXISTS users
-#           ([username] TEXT, [password] TEXT)
-#           ''')
-# conn.commit()
+# Create table
+c.execute('''
+          CREATE TABLE IF NOT EXISTS users
+          ([username] TEXT, [password] TEXT)
+          ''')
+conn.commit()
 
-# # Insert a sample user (this is just for demonstration; ideally, you'd have a registration process)
-# c.execute('''
-#           INSERT INTO users (username, password)
-#           VALUES ('admin', 'password123')
-#           ''')
-# conn.commit()
+# Insert a sample user (this is just for demonstration; ideally, you'd have a registration process)
+c.execute('''
+          INSERT INTO users (username, password)
+          VALUES ('admin', 'password123')
+          ''')
+conn.commit()
 
-# def verify_login(username, password):
-#     c.execute('''
-#               SELECT * FROM users WHERE username=? AND password=?
-#               ''', (username, password))
-#     return c.fetchone()
+def verify_login(username, password):
+    c.execute('''
+              SELECT * FROM users WHERE username=? AND password=?
+              ''', (username, password))
+    return c.fetchone()
 
 
-# def login():
-#     st.title("Login")
-#     username = st.text_input("Username")
-#     password = st.text_input("Password", type='password')
-#     if st.button("Login"):
-#         user = verify_login(username, password)
-#         if user:
-#             st.success("Logged in successfully!")
-#             st.session_state.logged_in = True
-#         else:
-#             st.error("Invalid username or password")
+def login():
+    st.title("Login")
+    username = st.text_input("Username")
+    password = st.text_input("Password", type='password')
+    if st.button("Login"):
+        user = verify_login(username, password)
+        if user:
+            st.success("Logged in successfully!")
+            st.session_state.logged_in = True
+        else:
+            st.error("Invalid username or password")
  
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
